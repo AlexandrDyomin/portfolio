0beffc666a98d4c91f11c8479b3753496a1c81b2
Alexandr Dyomin <alexandr.dyomin@yahoo.com>
2022-06-06 22:05:04 +0300

    update

---
+++ script.js
@@ -0,0 +1,10 @@
+let projects = document.querySelector(".projects");
+
+
+window.addEventListener("load", handleWindowLoad);
+
+
+function handleWindowLoad() {
+	projects.classList.add("projects_animated");
+}
+
